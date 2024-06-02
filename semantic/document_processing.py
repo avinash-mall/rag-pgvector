acb26b96c50ece73ca2ccf61e98bc23ac4ecc9ab
Avinash Mall <111482444+avinash-mall@users.noreply.github.com>
2024-06-02 23:37:50 +0400

    Update document_processing.py

--- semantic/document_processing.py
+++ semantic/document_processing.py
@@ -6,28 +6,22 @@ from llama_index.core import SimpleDirectoryReader
 import re
 from nltk.corpus import stopwords
 
-
-# Function to clean text
 def clean_text(text, language='en'):
     if language == 'en':
         text = text.lower()
         text = re.sub(r'[^a-z0-9\s]', '', text)
         text = ' '.join(word for word in text.split() if word not in stopwords.words('english'))
     elif language == 'ar':
-        text = re.sub(r'[^\u0600-\u06FF\s]', '', text)  # Retain Arabic characters and spaces
-        text = re.sub(r'\s+', ' ', text)  # Remove extra spaces
+        text = re.sub(r'[^\u0600-\u06FF\s]', '', text)
+        text = re.sub(r'\s+', ' ', text)
         text = text.strip()
         text = ' '.join(word for word in text.split() if word not in stopwords.words('arabic'))
     return text
 
-
 def get_meta(file_path):
-    # Retrieve file creation date, last modified date, and time
     creation_time = datetime.fromtimestamp(os.path.getctime(file_path)).isoformat()
     last_modified_time = datetime.fromtimestamp(os.path.getmtime(file_path)).isoformat()
-    # Get file name
     file_name = os.path.basename(file_path)
-    # Processed date and time
     processed_time = datetime.now().isoformat()
     return {
         "creation_date": creation_time,
@@ -36,12 +30,10 @@ def get_meta(file_path):
         "file_name": file_name
     }
 
-
 def read_documents_from_directory(directory):
     documents = []
     combined_documents = {}
 
-    # Initialize the reader with the specified parameters and metadata function
     reader = SimpleDirectoryReader(
         input_dir=directory,
         recursive=True,
@@ -50,7 +42,6 @@ def read_documents_from_directory(directory):
         file_metadata=get_meta
     )
 
-    # Iterate over the data returned by the reader
     for docs in reader.iter_data():
         for doc in docs:
             file_path = doc.metadata['file_name']
@@ -59,7 +50,6 @@ def read_documents_from_directory(directory):
             else:
                 combined_documents[file_path]["text"] += "\n" + doc.text
 
-    # Combine text from all pages of each file into a single document entry
     for combined in combined_documents.values():
         documents.append(combined)
 
